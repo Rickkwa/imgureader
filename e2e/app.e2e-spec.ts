194094d004c01943f1b3038ccd8d3872f3c86ab2
Angular CLI <angular-cli@angular.io>
2017-05-27 23:19:15 +0000

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { ImgureaderPage } from './app.po';
+
+describe('imgureader App', () => {
+  let page: ImgureaderPage;
+
+  beforeEach(() => {
+    page = new ImgureaderPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
