b1265ae8804319b7233dafa073898f1a200b6036
Ricky Kwan <rickyk.162@gmail.com>
2017-05-28 05:26:50 +0000

    Added placeholder files

---
+++ src/app/chapter.ts
@@ -0,0 +1,8 @@
+import { Page } from './page';
+
+// An ordered list of Pages with some meta data
+export class Chapter {
+    title: string;
+    pages: Page[];
+    // And whatever meta data from imgur api
+}
